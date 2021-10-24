85340fbd58366895785ea4547ac23ff7458002a7
Kai Sun <ksun0017@qq.com>
2021-10-24 21:49:51 +1100

    add state

--- c3fooderlich/lib/author_card.dart
+++ c3fooderlich/lib/author_card.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'fooderlich_theme.dart';
 import 'circle_image.dart';
 
-class AuthorCard extends StatelessWidget {
+class AuthorCard extends StatefulWidget {
   // 1
   final String authorName;
   final String title;
@@ -15,7 +15,12 @@ class AuthorCard extends StatelessWidget {
     this.imageProvider,
   }) : super(key: key);
 
-  // 2
+  @override
+  State<AuthorCard> createState() => _AuthorCardState();
+}
+
+class _AuthorCardState extends State<AuthorCard> {
+  bool _isFavourited = false;
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -27,7 +32,7 @@ class AuthorCard extends StatelessWidget {
           Row(
             children: [
               CircleImage(
-                imageProvider: imageProvider,
+                imageProvider: widget.imageProvider,
                 imageRadius: 28,
               ),
               // 2
@@ -37,11 +42,11 @@ class AuthorCard extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
-                    authorName,
+                    widget.authorName,
                     style: FooderlichTheme.lightTextTheme.headline2,
                   ),
                   Text(
-                    title,
+                    widget.title,
                     style: FooderlichTheme.lightTextTheme.headline3,
                   )
                 ],
@@ -49,13 +54,18 @@ class AuthorCard extends StatelessWidget {
             ],
           ),
           IconButton(
-              onPressed: () {
-                const snackBar = SnackBar(content: Text('Favourite Pressed'));
-                ScaffoldMessenger.of(context).showSnackBar(snackBar);
-              },
-              icon: const Icon(Icons.favorite_border),
-              iconSize: 30,
-              color: Colors.grey[400])
+            // 1
+            icon: Icon(_isFavourited ? Icons.favorite : Icons.favorite_border),
+            iconSize: 30,
+            // 2
+            color: Colors.red[400],
+            onPressed: () {
+              // 3
+              setState(() {
+                _isFavourited = !_isFavourited;
+              });
+            },
+          )
         ],
       ),
     );
