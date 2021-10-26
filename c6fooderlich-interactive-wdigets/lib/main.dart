c6a2b3b7238cc2886c1aa1633c0f99ae0da3f1c6
孙楷 (Kai Sun) <ksun0017@qq.com>
2021-10-26 22:52:00 +1100

    flutter interactive widgets

---
+++ c6fooderlich-interactive-wdigets/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:fooderlich/models/models.dart';
+import 'package:provider/provider.dart';
+
+import 'fooderlich_theme.dart';
+import 'home.dart';
+
+void main() {
+  runApp(const Fooderlich());
+}
+
+class Fooderlich extends StatelessWidget {
+  const Fooderlich({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = FooderlichTheme.light();
+    return MaterialApp(
+      theme: theme,
+      title: 'Fooderlich',
+      // TODO 8: Replace this with MultiProvider
+      home: MultiProvider(providers: [
+        ChangeNotifierProvider(create: (context)=>TabManager())
+      ],child: const Home(),)
+    );
+  }
+}
