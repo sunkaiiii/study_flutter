0b6976167d7e27928471527431993cb60641a446
孙楷 (Kai) <ksun0017@qq.com>
2021-11-03 00:41:06 +1100

    flutter stream

---
+++ c14stream/lib/data/repository.dart
@@ -0,0 +1,36 @@
+
+import 'models/models.dart';
+
+abstract class Repository {
+  Future<List<Recipe>> findAllRecipes();
+
+  Future<Recipe> findRecipeById(int id);
+
+  Future<List<Ingredient>> findAllIngredients();
+
+  // 1
+  Stream<List<Recipe>> watchAllRecipes();
+// 2
+  Stream<List<Ingredient>> watchAllIngredients();
+
+
+  Future<List<Ingredient>> findRecipeIngredients(int recipeId);
+
+  Future<int> insertRecipe(Recipe recipe);
+
+  Future<List<int>> insertIngredients(List<Ingredient> ingredients);
+
+  Future<void> deleteRecipe(Recipe recipe);
+
+  Future<void> deleteIngredient(Ingredient ingredient);
+
+  Future<void> deleteIngredients(List<Ingredient> ingredients);
+
+  Future<void> deleteRecipeIngredients(int recipeId);
+
+  Future init();
+
+  void close();
+
+
+}
